43caea8a64e4e56bdc797f5e1fbf192072d1ea24
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2025-02-09 11:51:24 -0600

    Removed debug

--- panzoom.js
+++ panzoom.js
@@ -9,7 +9,7 @@ class PanZoom {
     this.pmouse = { x: 0, y: 0 };
     this.keys = {};
     this.zoomToMouse = false;
-    this.debug = true;
+    this.debug = false;
     this.ignoreInput = 0;
     this.initEvents();
   }
